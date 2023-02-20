1a50fff42bb728f11f98fa69fca6f1078c41902d
Karina30 <63238336+Karina30@users.noreply.github.com>
2023-02-20 21:54:47 +0530

    Add files via upload

---
+++ 1Assig1QFibonacci.py
@@ -0,0 +1,10 @@
+#Python program to get the Fibonacci series between 0 to 50
+
+x,y = 0,1
+print("These are the fibonacci series no between 0 to 50:")
+while y<50:
+    print(y, end = " ")
+    x,y = y,x+y
+
+
+
